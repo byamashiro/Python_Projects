7fe855638a0f216334149b9b367a06d6f40e9027
Bryan Yamashiro <byamashiro09@gmail.com>
2017-07-27 22:31:28 -1000

    dg email addresses

--- data_generator.py
+++ data_generator.py
@@ -7,14 +7,24 @@ no_lines = '9'
 
 #dg_df = pd.DataFrame([])
 
+
+# ============= Name Column
 headers = ['fname', 'lname']
 dg_df = pd.read_csv('data/names.txt', names=headers, sep=' ')
 
+
+# =============  Datetime Column
+''' # Uncomment when program is complete
 start_date = input('Enter a start date (yyyymmdd): ')
 end_date = input('Enter an end date (yyyymmdd): ')
-
 start_hour = input('Enter a start hour or "full": ').zfill(2)
 end_hour = input('Enter an end hour or "full": ').zfill(2)
+'''
+
+start_date = '20120307'
+end_date = '20120310'
+start_hour = '00'
+end_hour = '00'
 
 start_datetime = datetime.datetime( year = int(start_date[0:4]), month = int(start_date[4:6]) , day = int(start_date[6:8]), hour = int(start_hour), minute = 0, second = 0 )
 end_datetime = datetime.datetime( year = int(end_date[0:4]), month = int(end_date[4:6]) , day = int(end_date[6:8]), hour = int(end_hour), minute = 0, second = 0 )
@@ -30,4 +40,15 @@ for i in range(int(no_lines)):
 	date_list.append(random_date(start_datetime, end_datetime))
 
 dg_df['date_time'] = sorted(date_list)
+
+
+# ============= Email domains
+email_domains = pd.read_csv('data/free_email_provider_domains.txt', names=['domain'], comment='#')
+
+email_list = []
+email_address = email_domains['domain'].tolist()
+for i in range(len(dg_df)):
+	email_list.append(random.choice(email_address))
+
+dg_df['email_address'] = dg_df.fname.astype(str).str[0] + dg_df.lname + '@' + email_list
 
